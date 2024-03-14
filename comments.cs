a969fe30525e1d740af155795a42fc189f42a682
Suraj Sahani <93707688+techsurajsahani@users.noreply.github.com>
2024-03-14 17:04:46 +0530

    14/03/2024

---
+++ comments.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace SurajSahani{
+    
+    class Program{
+        
+        static void Main(string[] s){
+            
+            // To output values or print text in C#, you can use the WriteLine() method
+            
+            Console.WriteLine("Disha Computer Institute");
+            Console.Write("My name is Suraj Sahani");
+            Console.Write("I am IT Trainer In Disha Computer Institute");
+            
+            /* The only difference is that Write() does not insert 
+            a new line at the end of the output */
+        }
+    }
+}
