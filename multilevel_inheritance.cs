3ae00842cff3d1950f61c64102c98ecbc75653aa
Suraj Sahani <93707688+techsurajsahani@users.noreply.github.com>
2024-03-14 18:55:36 +0530

    14/03/2024

---
+++ multilevel_inheritance.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace SurajSahani{
+    
+    public class Program{
+        
+        public static void Main(string[] s){
+          
+          A a=new A();
+          B b=new B();
+          C c=new C();
+          
+          a.display();
+          b.display();
+          c.display();
+        }
+    }
+    
+    class A{
+       public void display(){
+           Console.WriteLine("Class A");
+       } 
+    }
+    
+    class B : A{
+       public void display(){
+           Console.WriteLine("Class B");
+       } 
+    }
+    
+    class C : B{
+       public void display(){
+           Console.WriteLine("Class C");
+       } 
+    }
+
+}
