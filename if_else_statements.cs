a52c89e42c2f74cbea6139a8df779a6e82373fc0
Suraj Sahani <93707688+techsurajsahani@users.noreply.github.com>
2024-03-14 17:34:51 +0530

    14/03/2024

---
+++ if_else_statements.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace SurajSahani{
+    
+    class Program{
+        
+        static void Main(string[] s){
+            
+            // Conditional Statements 
+            
+            // If Else Statements
+            
+            int day=1;
+            
+            if(day==1){
+                Console.WriteLine("Monday");
+            }
+            else if(day==2){
+                Console.WriteLine("Tuesday");
+            }
+            else if(day==3){
+                Console.WriteLine("Wednesday");
+            }
+            else if(day==4){
+                Console.WriteLine("Thursday");
+            }
+            else if(day==5){
+                Console.WriteLine("Friday");
+            }
+            else if(day==6){
+                Console.WriteLine("Saturday");
+            }
+            else if(day==7){
+                Console.WriteLine("Sunday");
+            }
+            else{
+                Console.WriteLine("NOT KNOWN");
+            }
+            
+        }
+    }
+}
