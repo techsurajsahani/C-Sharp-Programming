c40be237ffd31cf6f3ce30e3e675a7bf050303a8
Suraj Sahani <93707688+techsurajsahani@users.noreply.github.com>
2024-03-14 17:17:51 +0530

    14/03/2024

---
+++ variables.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace SurajSahani{
+    
+    class Program{
+        
+        static void Main(string[] s){
+            
+            int a=5;
+            float f=5.5f;
+            char c='s';
+            string str="Disha Computer Institute";
+            
+            Console.WriteLine(a);
+            Console.WriteLine(f);
+            Console.WriteLine(c);
+            Console.WriteLine(str);
+            
+        }
+    }
+}
