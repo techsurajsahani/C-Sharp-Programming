a18a5eb719e21e7b1229f79864c98507b8691bd1
Suraj Sahani <93707688+techsurajsahani@users.noreply.github.com>
2024-03-14 16:56:37 +0530

    14/03/2024

---
+++ basic.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SurajSahani{
+    
+    class Program{
+        
+        static void Main(string[] s){
+            
+            Console.WriteLine("Disha Computer Institute");
+        }
+    }
+}
