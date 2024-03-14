b64317934dcdcbe302610aef6116a3bfdfea681f
Suraj Sahani <93707688+techsurajsahani@users.noreply.github.com>
2024-03-14 16:59:54 +0530

    14/03/2024

---
+++ how_to_print.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace SurajSahani{
+    
+    class Program{
+        
+        static void Main(string[] s){
+            
+            Console.WriteLine("Disha Computer Institute");
+            Console.Write("My name is Suraj Sahani");
+            Console.Write("I am IT Trainer In Disha Computer Institute");
+        }
+    }
+}
